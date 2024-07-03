a434e24421d50ba384d0cd4c47a15b816577e40d
readiz <readiz@readiz.com>
2024-07-04 01:48:17 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 function BAPlayer() {
-  
-
+  let wavesurfer = null;
 /*
 Example: https://www.codehim.com/demo/javascript-audio-player-with-playlist/
 */
@@ -49,92 +48,75 @@ function formatTime(val) {
   return val;
 }
 
-function simp_initTime() {
-  simp_controls.querySelector('.start-time').innerHTML = formatTime(simp_audio.currentTime); //calculate current value time
+function simp_initTime(curTime) {
+  let duration = wavesurfer.getDuration();
+
+  simp_controls.querySelector('.start-time').innerHTML = formatTime(curTime); //calculate current value time
   if (!simp_isStream) {
-    simp_controls.querySelector('.end-time').innerHTML = formatTime(simp_audio.duration); //calculate total value time
-    simp_progress.value = simp_audio.currentTime / simp_audio.duration * 100; //progress bar
-  }
-  
-  // ended of the audio
-  if (simp_audio.currentTime == simp_audio.duration) {
-    simp_controls.querySelector('.simp-plause').classList.remove('fa-pause');
-    simp_controls.querySelector('.simp-plause').classList.add('fa-play');
-    simp_audio.removeEventListener('timeupdate', simp_initTime);
-    
-    if (simp_isNext) { //auto load next audio
-      var elem;
-      simp_a_index++;
-      if (simp_a_index == simp_a_url.length) { //repeat all audio
-        simp_a_index = 0;
-        elem = simp_a_url[0];
-      } else {
-        elem = simp_a_url[simp_a_index];  
-      }
-      simp_changeAudio(elem);
-      simp_setAlbum(simp_a_index);
-    } else {
-      simp_isPlaying = false;
-    }
+    simp_controls.querySelector('.end-time').innerHTML = formatTime(duration); //calculate total value time
+    simp_progress.value = curTime / duration * 100; //progress bar
   }
 }
 
-function simp_initAudio() {
+function simp_initAudio(isFirst = false) {
+  console.log('init', isFirst);
   // if readyState more than 2, audio file has loaded
-	simp_isLoaded = simp_audio.readyState == 4 ? true : false;
-  simp_isStream = simp_audio.duration == 'Infinity' ? true : false;
+	// simp_isLoaded = simp_audio.readyState == 4 ? true : false;
+  simp_isLoaded = true;
+  // simp_isStream = simp_audio.duration == 'Infinity' ? true : false;
+  simp_isStream = false;
   simp_controls.querySelector('.simp-plause').disabled = false;
-  simp_progress.disabled = simp_isStream ? true : false;
-  if (!simp_isStream) {
-    simp_progress.parentNode.classList.remove('simp-load','simp-loading');
-    simp_controls.querySelector('.end-time').innerHTML = formatTime(simp_audio.duration);
-  }
-  simp_audio.addEventListener('timeupdate', simp_initTime); //tracking load progress
-  if (simp_isLoaded) {
-    if (simp_isPlaying) simp_audio.play();
-    else {
-      togglePlayPause();
-    }
+  // simp_progress.disabled = simp_isStream ? true : false;
+  //if (!simp_isStream) {
+  simp_progress.parentNode.classList.remove('simp-load','simp-loading');
+  //simp_controls.querySelector('.end-time').innerHTML = formatTime(wavesurfer.duration);
+  //}
+  // simp_audio.addEventListener('timeupdate', simp_initTime); //tracking load progress
+  if (isFirst != true && simp_isLoaded) {
+    togglePlayPause(null, true);
   }
   
   // progress bar click event
   addEventListener_multi(simp_progress, 'touchstart mousedown', function(e) {
-    if (simp_isStream) {
-      e.stopPropagation();
-      return false;
-    }
-    if (simp_audio.readyState == 4) {
-      simp_audio.removeEventListener('timeupdate', simp_initTime);
-      simp_audio.pause();
-    }
+    // if (simp_isStream) {
+    //   e.stopPropagation();
+    //   return false;
+    // }
+    // if (simp_audio.readyState == 4) {
+    //   simp_audio.removeEventListener('timeupdate', simp_initTime);
+    //   simp_audio.pause();
+    // }
+    wavesurfer.pause();
   });
   
   addEventListener_multi(simp_progress, 'touchend mouseup', function(e) {
-    if (simp_isStream) {
-      e.stopPropagation();
-      return false;
-    }
-    if (simp_audio.readyState == 4) {
-      simp_audio.currentTime = simp_progress.value * simp_audio.duration / 100;
-      simp_audio.addEventListener('timeupdate', simp_initTime);
-      if (simp_isPlaying) simp_audio.play();
-    }
+    // if (simp_isStream) {
+    //   e.stopPropagation();
+    //   return false;
+    // }
+    //if (simp_audio.readyState == 4) {
+      //simp_audio.currentTime = simp_progress.value * simp_audio.duration / 100;
+      //simp_audio.addEventListener('timeupdate', simp_initTime);
+      wavesurfer.seekTo(simp_progress.value / 100);
+      if (simp_isPlaying) wavesurfer.play(); //simp_audio.play();
+    //}
   });
 }
 
 function simp_loadAudio(elem) {
+  console.log('load');
   simp_progress.parentNode.classList.add('simp-loading');
   simp_controls.querySelector('.simp-plause').disabled = true;
-  simp_audio.querySelector('source').src = elem.dataset.src;
-  simp_audio.load();
+  //simp_audio.querySelector('source').src = elem.dataset.src;
+  //simp_audio.load();
   
-  simp_audio.volume = parseFloat(simp_v_num / 100); //based on valume input value
-  simp_audio.addEventListener('canplaythrough', simp_initAudio); //play audio without stop for buffering
+  //simp_audio.volume = parseFloat(simp_v_num / 100); //based on valume input value
+  //simp_audio.addEventListener('canplaythrough', simp_initAudio); //play audio without stop for buffering
   
   // if audio fails to load, only IE/Edge 9.0 or above
-  simp_audio.addEventListener('error', function() {
-    alert('Please reload the page.');
-  });
+  // simp_audio.addEventListener('error', function() {
+  //   alert('Please reload the page.');
+  // });
 
   
   navigator.mediaSession.setActionHandler("previoustrack", prevHandler);
@@ -149,7 +131,7 @@ function simp_setAlbum(index) {
   simp_artist.innerHTML = simp_source[index].querySelector('.simp-desc') ? simp_source[index].querySelector('.simp-desc').innerHTML : '';
 }
 
-function simp_changeAudio(elem) {
+function simp_changeAudio(elem, isFirst = false) {
 	simp_isLoaded = false;
   simp_controls.querySelector('.simp-prev').disabled = simp_a_index == 0 ? true : false;
   simp_controls.querySelector('.simp-plause').disabled = simp_auto_load ? true : false;
@@ -160,7 +142,7 @@ function simp_changeAudio(elem) {
   simp_controls.querySelector('.start-time').innerHTML = '0:00';
   simp_controls.querySelector('.end-time').innerHTML = '0:00';
   elem = simp_isRandom && simp_isNext ? simp_a_url[getRandom(0, simp_a_url.length-1)] : elem;
-  
+
   // playlist, audio is running
   for (var i = 0; i < simp_a_url.length; i++) {
     simp_a_url[i].parentNode.classList.remove('simp-active');
@@ -181,21 +163,73 @@ function simp_changeAudio(elem) {
     simp_controls.querySelector('.simp-plause').classList.add('fa-pause');
   }
 
-  
+  if (wavesurfer) wavesurfer.destroy();
+  wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#5f5f5f',
+    progressColor: '#ffffff',
+    height: 70,
+    url: elem.dataset.src,
+  })
+
+  wavesurfer.on('interaction', () => {
+    togglePlayPause(null, true);
+  })
+  wavesurfer.on('timeupdate', (currentTime) => {
+    //console.log('Time', currentTime + 's')
+    simp_initTime(currentTime);
+  })
+  wavesurfer.on('ready', () =>{
+    console.log('song ready!');
+    simp_initAudio(isFirst);
+    simp_progress.disabled = false;
+  })
+  wavesurfer.on('finish', () => {
+    console.log('Song End!!');
+    if (simp_isRepeat) {
+      wavesurfer.seekTo(0);
+      wavesurfer.play();
+      return;
+    }
+
+    simp_controls.querySelector('.simp-plause').classList.remove('fa-pause');
+    simp_controls.querySelector('.simp-plause').classList.add('fa-play');
+    // simp_audio.removeEventListener('timeupdate', simp_initTime);
+    
+    if (simp_isNext) { //auto load next audio
+      var elem;
+      simp_a_index++;
+      if (simp_a_index == simp_a_url.length) { //repeat all audio
+        simp_a_index = 0;
+        elem = simp_a_url[0];
+      } else {
+        elem = simp_a_url[simp_a_index];  
+      }
+      simp_changeAudio(elem);
+      simp_setAlbum(simp_a_index);
+    } else {
+      simp_isPlaying = false;
+    }
+  });
 }
 
-function togglePlayPause(eles) {
+function togglePlayPause(eles, forcePlay = false) {
+  console.log('toggle', forcePlay);
   if (!eles) {
     eles = document.querySelector('.simp-plause').classList;
   }
-  if (simp_audio.paused) {
+  if (simp_isPlaying == false || forcePlay) {
     if (!simp_isLoaded) simp_loadAudio(simp_a_url[simp_a_index]);
-    simp_audio.play();
+    // simp_audio.play();
+    wavesurfer.play();
     simp_isPlaying = true;
     eles.remove('fa-play');
-    eles.add('fa-pause');
+    eles.add('fa-pause')
+    console.log('play!');
   } else {
-    simp_audio.pause();
+    //simp_audio.pause();
+    console.log('pause!');
+    wavesurfer.pause();
     simp_isPlaying = false;
     eles.remove('fa-pause');
     eles.add('fa-play');
@@ -234,7 +268,7 @@ function simp_startScript() {
   simp_source.forEach(function(item, index) {
     if (item.classList.contains('simp-active')) simp_a_index = index; //playlist contains '.simp-active'
     item.addEventListener('click', function() {
-      simp_audio.removeEventListener('timeupdate', simp_initTime);
+      //simp_audio.removeEventListener('timeupdate', simp_initTime);
       simp_a_index = index;
       simp_changeAudio(this.querySelector('.simp-source'));
       simp_setAlbum(simp_a_index);
@@ -242,7 +276,7 @@ function simp_startScript() {
   });
   
   // FIRST AUDIO LOAD =======
-  simp_changeAudio(simp_a_url[simp_a_index]);
+  simp_changeAudio(simp_a_url[simp_a_index], true);
   simp_setAlbum(simp_a_index);
   // FIRST AUDIO LOAD =======
   
@@ -259,41 +293,41 @@ function simp_startScript() {
         simp_a_index = simp_a_index+1;
         e.target.disabled = simp_a_index == simp_a_url.length-1 ? true : false;
       }
-      simp_audio.removeEventListener('timeupdate', simp_initTime);
+      //simp_audio.removeEventListener('timeupdate', simp_initTime);
       simp_changeAudio(simp_a_url[simp_a_index]);
       simp_setAlbum(simp_a_index);
     }
   });
   
   // Audio volume
-  simp_volume.addEventListener('click', function(e) {
-    var eles = e.target.classList;
-    if (eles.contains('simp-mute')) {
-      if (eles.contains('fa-volume-up')) {
-        eles.remove('fa-volume-up');
-        eles.add('fa-volume-off');
-        simp_v_slider.value = 0;
-      } else {
-        eles.remove('fa-volume-off');
-        eles.add('fa-volume-up');
-        simp_v_slider.value = simp_v_num;
-      }
-    } else {
-      simp_v_num = simp_v_slider.value;
-      if (simp_v_num != 0) {
-        simp_controls.querySelector('.simp-mute').classList.remove('fa-volume-off');
-        simp_controls.querySelector('.simp-mute').classList.add('fa-volume-up');
-      }
-    }
-    simp_audio.volume = parseFloat(simp_v_slider.value / 100);
-  });
+  // simp_volume.addEventListener('click', function(e) {
+  //   var eles = e.target.classList;
+  //   if (eles.contains('simp-mute')) {
+  //     if (eles.contains('fa-volume-up')) {
+  //       eles.remove('fa-volume-up');
+  //       eles.add('fa-volume-off');
+  //       simp_v_slider.value = 0;
+  //     } else {
+  //       eles.remove('fa-volume-off');
+  //       eles.add('fa-volume-up');
+  //       simp_v_slider.value = simp_v_num;
+  //     }
+  //   } else {
+  //     simp_v_num = simp_v_slider.value;
+  //     if (simp_v_num != 0) {
+  //       simp_controls.querySelector('.simp-mute').classList.remove('fa-volume-off');
+  //       simp_controls.querySelector('.simp-mute').classList.add('fa-volume-up');
+  //     }
+  //   }
+  //   simp_audio.volume = parseFloat(simp_v_slider.value / 100);
+  // });
   
   // Others
   simp_others.addEventListener('click', function(e) {
     var eles = e.target.classList;
     if (eles.contains('simp-repeat')) {
       simp_isRepeat = !simp_isRepeat;
-      simp_audio.loop = simp_isRepeat;
+      // simp_audio.loop = simp_isRepeat;
       eles.contains('simp-active') && !simp_isRepeat ? eles.remove('simp-active') : eles.add('simp-active');
     } else if (eles.contains('simp-plext')) {
       simp_isNext = simp_isNext && !simp_isRandom ? false : true;
@@ -342,6 +376,7 @@ if (document.querySelector('#simp')) {
   var simp_elem = '';
   simp_elem += '<audio id="audio" preload><source src="" type="audio/mpeg"></audio>';
   simp_elem += '<div class="simp-display"><div class="simp-album w-full flex-wrap"><div class="simp-cover"><i class="fa fa-music fa-5x"></i></div><div class="simp-info"><div class="simp-title">Title</div><div class="simp-artist">Artist</div></div></div></div>';
+  simp_elem += '<div id="waveform" style="width:calc(100%-40px);height:50px;margin:20px;"></div>'
   simp_elem += '<div class="simp-controls flex-wrap flex-align">';
   simp_elem += '<div class="simp-plauseward flex flex-align"><button type="button" class="simp-prev fa fa-backward" disabled></button><button type="button" class="simp-plause fa fa-play" disabled></button><button type="button" class="simp-next fa fa-forward" disabled></button></div>';
   simp_elem += '<div class="simp-tracker simp-load"><input class="simp-progress" type="range" min="0" max="100" value="0" disabled/><div class="simp-buffer"></div></div>';
